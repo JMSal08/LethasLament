c29eae89152ee3864c65658f8fc35f1e8f7f263b
JMSal08 <jmsal08@gmail.com>
2025-10-26 23:15:04 +0800

    Additional Changes to Game Splash
    Adjustments

--- scripts/game_splash.gd
+++ scripts/game_splash.gd
@@ -2,4 +2,5 @@ extends Control
 
 
 func _on_ready() -> void:
-	pass # Replace with function body.
+	await get_tree().create_timer(2).timeout
+	TransitionScene.change_scene_to_file("res://scenes/main_menu.tscn")
