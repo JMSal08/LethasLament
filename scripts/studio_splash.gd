522b7d2637adc40a6d0327822420b0a206887ea1
JMSal08 <jmsal08@gmail.com>
2025-10-27 00:59:42 +0800

    Added click to skip in studio_splash scene

--- scripts/studio_splash.gd
+++ scripts/studio_splash.gd
@@ -11,3 +11,8 @@ func _on_ready() -> void:
 func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 	await get_tree().create_timer(2.5).timeout
 	TransitionScene.change_scene_to_file("res://scenes/game_splash.tscn")
+
+
+func _on_overlay_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		TransitionScene.change_scene_to_file("res://scenes/game_splash.tscn")
