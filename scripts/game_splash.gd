d052945d8156cc6f113c80b018b0657d5b131000
JMSal08 <jmsal08@gmail.com>
2025-10-27 00:58:39 +0800

    Removed GIF Entirely
    Removed the gif for a manually animated scene.

--- scripts/game_splash.gd
+++ scripts/game_splash.gd
@@ -3,4 +3,9 @@ extends Control
 
 func _on_ready() -> void:
 	await get_tree().create_timer(2).timeout
-	TransitionScene.change_scene_to_file("res://scenes/main_menu.tscn")
+	TransitionScene.change_scene_to_file("res://scenes/warning_scene.tscn")
+
+
+func _on_overlay_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		TransitionScene.change_scene_to_file("res://scenes/warning_scene.tscn")
