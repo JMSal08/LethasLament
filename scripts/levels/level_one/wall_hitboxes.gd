41abb0f2293c5ab01df06f05c83082191e5cb0e1
JMSal08 <jmsal08@gmail.com>
2025-11-09 14:28:19 +0800

    Letha Adjustments and Instructions Panel

--- scripts/levels/level_one/wall_hitboxes.gd
+++ scripts/levels/level_one/wall_hitboxes.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var target_loc = Vector2(92, 112)
+@export var target_loc = Vector2(91, 123)
 
 @onready var letha = $"../Letha"
 func _on_body_entered(_body: Node2D) -> void:
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var AnimPlayer = $AnimationPlayer
+@onready var Instructions = $Instructions
 
 func _on_ready() -> void:
 	AnimPlayer.play("play")
@@ -8,3 +9,11 @@ func _on_ready() -> void:
 func _on_play_button_pressed() -> void:
 	TransitionScene.change_scene_to_file("res://scenes/levels/level_one.tscn")
 	AudioPlayer.playLevelOne()
+
+
+func _on_instructions_button_pressed() -> void:
+	Instructions.visible = true
+
+
+func _on_ins_close_pressed() -> void:
+	Instructions.visible = false
