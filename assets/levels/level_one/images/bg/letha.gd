9de71ed8914d4b60b5326e1599e042dfea190056
JMSal08 <jmsal08@gmail.com>
2025-11-16 22:35:48 +0800

    Changed Level One Finish
    Changed Image

--- assets/levels/level_one/images/bg/letha.gd
+++ assets/levels/level_one/images/bg/letha.gd
@@ -6,11 +6,17 @@ extends CharacterBody2D
 
 func _physics_process(_delta: float) -> void:
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	#print(input_direction)
+	#print(input_direction) # for debug only
 	velocity = input_direction * speed
 	if input_direction == Vector2(0,1):
 		animated_sprite.play("down")
+	elif input_direction == Vector2(0,-1):
+		pass #Put animated up here
+	elif input_direction == Vector2(1,0):
+		pass # Put animated right here
+	elif  input_direction == Vector2(-1,0):
+		pass # Put animated left here
 	else:
-		animated_sprite.play("default")
+		animated_sprite.play("default") # Plays default animation
 	
 	move_and_slide()
