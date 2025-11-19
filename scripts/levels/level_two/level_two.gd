22adf5ce394da6b90dfdbbc50defff0719fd3d24
JMSal08 <jmsal08@gmail.com>
2025-11-20 00:05:37 +0800

    Level two changes

--- scripts/levels/level_two/level_two.gd
+++ scripts/levels/level_two/level_two.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @onready var Letha = $Letha
+@onready var SpeechBox = $SpeechBox
+@onready var Warning = $SpeechBox/Warning
 
 @onready var LevelTwoFinish = $LevelTwoFinish
 
@@ -8,9 +10,32 @@ extends Node2D
 @onready var Letter_O = $Letter_O
 @onready var Letter_G = $Letter_G
 
+var wall_touches = 0
+
 var has_D = false
 var has_O = false
 var has_G = false
+
+func _on_wall_hitboxes_body_entered(_body: Node2D) -> void:
+	wall_touches += 1
+	print(wall_touches)
+	if wall_touches == 3:
+		print("3 commenced")
+		Warning.text = "I shouldn't touch the walls..."
+		SpeechBox.show()
+		await get_tree().create_timer(3).timeout
+		SpeechBox.hide()
+		Warning.text = ""
+	elif wall_touches == 5:
+		Warning.text = "I seriously shouldn't."
+		SpeechBox.show()
+		await get_tree().create_timer(3).timeout
+		SpeechBox.hide()
+	elif wall_touches >= 7:
+		Warning.text = "..."
+		SpeechBox.show()
+		await get_tree().create_timer(3).timeout
+		SpeechBox.hide()
 
 func _on_letter_d_body_entered(_body: Node2D) -> void:
 	has_D = true
